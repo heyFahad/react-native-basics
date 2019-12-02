b75ca4e24d6b79ee39b86fedc84decc3364d1cef
Fahad Javed <Fahad_Javed008@yahoo.com>
2019-12-02 18:42:20 +0500

    Working with core components, & getting started with Styles in React Native

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ padding: 50 }}>
+      <View>
+        <TextInput
+          placeholder="Course Goal"
+          style={{ borderColor: 'black', borderWidth: 1, padding: 10 }}
+        />
+        <Button title="ADD" />
+      </View>
+      <View></View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
